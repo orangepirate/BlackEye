93de05bb7d0367c129279ffe94e4e331356bd19c
orangepirate <40813235+orangepirate@users.noreply.github.com>
2018-08-30 20:34:59 -0700

    add insert function

--- update_database.py
+++ update_database.py
@@ -6,12 +6,42 @@ class MySqlCommand(object):
         self.host = 'localhost'
         self.port = 3306
         self.user = 'root'
-        self.password = 'geniusjohn'
+        self.password = 'geniusroot'
         self.db = 'blackEye'
     # connect to database
     def connectMySql(self):
         try:
-            self.conn = pymysql.connect(self.host,self.port,self.user,self.password,self.db,charset='utf8')
+            self.conn = pymysql.connect(host=self.host,port=self.port,user=self.user,
+                                        passwd=self.password,db=self.db,charset='utf8')
             self.cursor = self.conn.cursor()
         except Exception as e:
             print(e)
+
+    def insertData(self,mydict):
+        table = 'devs'
+        sqlSelect = 'select dev_ip from {} where dev_ip = {}'.format(table,mydict['dev_ip'])
+        response = self.cursor.execute(sqlSelect)
+        if response:
+            print(' the dev {} is already exist'.format(mydict['dev_ip']))
+            return 0
+        try:
+            cols = ', '.join(mydict.keys())
+            values = '","'.join(mydict.values())
+            sqlInsert = 'insert into {} {} values {}'.format(table, cols, values)
+            try:
+                result = self.cursor.execute(sqlInsert)
+                insert_id = self.conn.insert_id()
+                self.conn.commit()
+                if result:
+                    print('insert success, the id is {}.'.format(insert_id))
+                    return insert_id+1
+            except pymysql.Error as e:
+                # rollback if something goes wrong
+                self.conn.rollback()
+                # check primary key
+                if "key 'PRIMARY'" in e.args[1]:
+                    print(' the data is already exist.')
+                else:
+                    print('insert failed, {} : {}'.format(e.args[0],e.args[1]))
+        except pymysql.Error as e:
+            print('insert failed, {} : {}'.format(e.args[0],e.args[1]))
